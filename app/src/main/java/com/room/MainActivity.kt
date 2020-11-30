a651dd060848c3fccb26f8d2f37a590532fcab1c
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-30 16:43:48 +0700

    x

--- app/src/main/java/com/room/MainActivity.kt
+++ app/src/main/java/com/room/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.room.view.HomeActivity
+import com.room.view.LoginActivity
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    val intent = Intent(this, HomeActivity::class.java)
+    val intent = Intent(this, LoginActivity::class.java)
     startActivity(intent)
   }
 }
