abde27ef08c1c64be260f9c2a9a280c5f91dfa15
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-01 15:48:53 +0700

    Update Loading.kt

--- app/src/main/java/com/room/config/Loading.kt
+++ app/src/main/java/com/room/config/Loading.kt
@@ -1,3 +1,26 @@
 package com.room.config
 
-class Loading {}
+import android.R.style.Theme_Translucent_NoTitleBar
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Dialog
+import com.room.R
+
+@SuppressLint("InflateParams")
+class Loading(private val activity: Activity) {
+  private val dialog = Dialog(activity, Theme_Translucent_NoTitleBar)
+
+  init {
+    val view = activity.layoutInflater.inflate(R.layout.activity_main, null)
+    dialog.setContentView(view)
+    dialog.setCancelable(false)
+  }
+
+  fun openDialog() {
+    dialog.show()
+  }
+
+  fun closeDialog() {
+    dialog.dismiss()
+  }
+}
